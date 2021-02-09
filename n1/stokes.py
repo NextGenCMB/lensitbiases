09660215f003c0b3994f2c4eca060790cd4c0311
jcarron <to.jcarron@gmail.com>
2021-02-09 19:57:03 +0100

    misc

--- n1/stokes.py
+++ n1/stokes.py
@@ -72,7 +72,7 @@ class stokes:
 
 
         self._cos2p_sin2p = None
-
+        self._cos2p_sin2p_v1 = None
         #==== Builds required spectra:
         # === Filter and cls array needed later on:
         fals   = {k: extcl(self.box.lmaxbox + lminbox, fals[k]) for k in fals.keys()}
@@ -90,15 +90,16 @@ class stokes:
         self.fFw_ls = cls_dot([cls_f, fals, cls_w])
         self.wFf_ls = cls_dot([cls_w, fals, cls_f])
 
+
         # === precalc of deflection corr fct:
         ny, nx = np.meshgrid(self.box.ny_1d, self.box.nx_1d, indexing='ij')
         ls = self.box.ls()
 
         self.xipp = np.zeros((2, 2, self.shape[0], self.shape[1]))
         self.xipp[0,0] = np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * ny ** 2)  # 00
-        self.xipp[1,0]= np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * nx * ny)  # 01 or 10
+        self.xipp[1,0]=  np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * nx * ny)  # 01 or 10
         self.xipp[0,1] = np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * nx * ny)  # 01 or 10
-        self.xipp[1,1] = np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * ny ** 2)  # 01 or 10
+        self.xipp[1,1] = np.fft.irfft2(extcl(self.box.lmaxbox, -cpp)[ls] * nx ** 2)  # 01 or 10
 
         del nx, ny, ls
 
@@ -110,13 +111,14 @@ class stokes:
 
 
     @staticmethod
-    def cos2p_sin2p(ly, lx):
+    def cos2p_sin2p(ls):
         """Returns the cosines and sines of twice the angle of the map of vectors
 
             Note:
                 This assumes no vector is zero
 
         """
+        ly, lx = ls
         r_sqd = lx ** 2 + ly ** 2
         return 2 * lx ** 2 / r_sqd - 1., 2 * lx * ly / r_sqd
 
@@ -141,6 +143,12 @@ class stokes:
         self.l1_int = self.box.rsqd2l(np.sum(l1s ** 2, axis=0))
         self.l2_int = self.box.rsqd2l(np.sum(l2s ** 2, axis=0))
         self.cos2p_sin2p_2v()
+        #if k in ['p_p', 'p']:
+        #    self.X2S('Q', 'E', 1)
+
+    def _destroy_key(self, k):
+        self._cos2p_sin2p_v1 = None
+        self._cos2p_sin2p = None
 
     def cos2p_sin2p_2v(self):
         """Returns the cosines and sines of twice the angle between the two maps of vectors
@@ -160,26 +168,26 @@ class stokes:
             self._cos2p_sin2p = (cos2p, sin2p)
         return self._cos2p_sin2p
 
-    def X2S(self, S, X, vec_1or2):
+    def X2S(self, S, X, vec1or2):
         """Matrix element sending X cmb mode to stokes flat-sky mode S
 
 
         """
-        assert S in ['T', 'Q', 'U'], S
-        assert X in ['T', 'E', 'B'], X
-        assert vec_1or2 in [1, 2], vec_1or2
-
-        ly, lx = self.l1s if vec_1or2 == 1 else self.l2s
         if S == 'T':
             return 1. if X == 'T' else 0.
         if S == 'Q':
-            if X == 'E': return self.cos2p_sin2p(ly, lx)[0]
-            if X == 'B': return -self.cos2p_sin2p(ly, lx)[1]
-            return 0.
+            if X == 'T':
+                return 0.
+            if self._cos2p_sin2p_v1 is None:
+                self._cos2p_sin2p_v1 = {1:  self.cos2p_sin2p(self.l1s), 2: self.cos2p_sin2p(self.l2s)}
+            sgn = 1 if X == 'E' else -1
+            return sgn * self._cos2p_sin2p_v1[vec1or2][0 if X == 'E' else 1]
         if S == 'U':
-            if X == 'E': return self.cos2p_sin2p(ly, lx)[1]
-            if X == 'B': return self.cos2p_sin2p(ly, lx)[0]
-            return 0.
+            if X == 'T':
+                return 0.
+            if self._cos2p_sin2p_v1 is None:
+                self._cos2p_sin2p_v1 = {1: self.cos2p_sin2p(self.l1s), 2: self.cos2p_sin2p(self.l2s)}
+            return self._cos2p_sin2p_v1[vec1or2][1 if X == 'E' else 0]
         assert 0
 
     def X2Y(self, Y, X):
@@ -196,7 +204,63 @@ class stokes:
             return self.cos2p_sin2p_2v()[0] # same as c1 * c1 + s1 * s2 = cos2p_12
         return (1 if X == 'B' else -1) * self.cos2p_sin2p_2v()[1]# same as -+ (c1 s2 - s1 c2) = -+ sin_2p_12:
 
-    def W_ST(self, S, T,  ders_1=None, ders_2=None, verbose=False):
+    def W_ST_Pol(self, ders_1=None, ders_2=None, verbose=False, rfft=False):
+        """Same as W_ST but returns all Stokes weights in one go
+
+            Returns: QQ, UU , QU and UQ
+
+        """
+        W1 = np.zeros((4, self.shape[0], self.shape[1]), dtype=float)  # terms of QE with Cl weight on l1 leg
+        W2 = np.zeros((4, self.shape[0], self.shape[1]), dtype=float)  # terms of QE with Cl weight on l1 leg
+        X2i = {'T': 0, 'E': 1, 'B': 2}
+        for X in ['E', 'B']:
+            #RSX_1 = self.X2S(S, X, 1)
+            #for Y in ['E', 'B']: # Assuming no EB and BE, Y must be X
+            Y = X
+            if ders_1 is not None:
+                cl_XY_1 = self.fFw_ls[X2i[X], X2i[Y]][self.l1_int] * (self.l1s[ders_1])  # wFf transpose..
+                cl_XY_2 = self.fF_ls[X2i[X], X2i[Y]][self.l1_int] * (self.l1s[ders_1])
+            else:
+                cl_XY_1 = self.Fw_ls[X2i[X], X2i[Y]][self.l1_int]
+                cl_XY_2 = self.F_ls[X2i[X], X2i[Y]][self.l1_int]
+            for Xp in ['E', 'B']:
+                RtR_YXp = self.X2Y(Y, Xp)
+                #for Yp in ['E', 'B']: # Assuming no EB and BE, Yp must be Xp
+                Yp = Xp
+                if ders_2 is not None:
+                    cl_XpYp_1 = self.Ff_ls[X2i[Xp], X2i[Yp]][self.l2_int] * (self.l2s[ders_2])
+                    cl_XpYp_2 = self.wFf_ls[X2i[Xp], X2i[Yp]][self.l2_int] * (self.l2s[ders_2])
+
+                else:
+                    cl_XpYp_1 = self.F_ls[X2i[Xp], X2i[Yp]][self.l2_int]
+                    cl_XpYp_2 = self.wF_ls[X2i[Xp], X2i[Yp]][self.l2_int]
+
+                #RTYp_2 = self.X2S(T, Yp, 2)
+
+                # Check ordering of X Y Xp Yp: ( XY or YX, XpYp or YpXp ?)
+                term1 = RtR_YXp * cl_XY_1 * cl_XpYp_1
+                term2 = RtR_YXp * cl_XY_2 * cl_XpYp_2
+
+                W1[0] += self.X2S('Q', X, 1)  * self.X2S('Q', Yp, 2) * term1
+                W1[1] += self.X2S('U', X, 1)  * self.X2S('U', Yp, 2) * term1
+                W1[2] += self.X2S('Q', X, 1)  * self.X2S('U', Yp, 2) * term1
+                W1[3] += self.X2S('U', X, 1)  * self.X2S('Q', Yp, 2) * term1
+
+                W2[0] += self.X2S('Q', X, 1)  * self.X2S('Q', Yp, 2) * term2
+                W2[1] += self.X2S('U', X, 1)  * self.X2S('U', Yp, 2) * term2
+                W2[2] += self.X2S('Q', X, 1)  * self.X2S('U', Yp, 2) * term2
+                W2[3] += self.X2S('U', X, 1)  * self.X2S('Q', Yp, 2) * term2
+                if verbose and np.any(term1):
+                    print('term1 ' + X + Y + ' ' + Xp + Yp)
+                if verbose and np.any(term2):
+                    print('term2 ' + X + Y + ' ' + Xp + Yp)
+
+        W1 *= np.sum((self.l1s + self.l2s) * self.l1s, axis=0)
+        W2 *= np.sum((self.l2s + self.l1s) * self.l2s, axis=0)
+        i_sign = 1j ** (ders_1 is not None) * 1j ** (ders_2 is not None)
+        return i_sign * (W1 + W2)  # , W1, W2
+
+    def W_ST(self, S, T,  ders_1=None, ders_2=None, verbose=False, Ponly=False):
         """Stokes QE weight function for Stokes parameter S T
 
             Note:
@@ -210,14 +274,13 @@ class stokes:
         assert ders_1 in [None, 0, 1], ders_1  # axis of derivative and Cl factor on first leg if relevant
         assert ders_2 in [None, 0, 1], ders_2  # axis of derivative and Cl factor on first leg if relevant
 
-        # (qpl[0] * qml[0] + qpl[1] * qml[1])
         W1 = np.zeros(self.shape, dtype=float)  # terms of QE with Cl weight on l1 leg
         W2 = np.zeros(self.shape, dtype=float)  # terms of QE with Cl weight on l2 leg
-
+        Xs = ['T'] * (not Ponly) + ['E', 'B']
         X2i = {'T' : 0, 'E' : 1, 'B' : 2}
         for X in ['T'] if S == 'T' else ['E', 'B']:
             RSX_1 = self.X2S(S, X, 1)
-            for Y in ['T', 'E', 'B']:
+            for Y in Xs:
                 if ders_1 is not None:
                     cl_XY_1 = self.fFw_ls[X2i[X], X2i[Y]][self.l1_int] * (self.l1s[ders_1]) # wFf transpose..
                     cl_XY_2 = self.fF_ls[ X2i[X], X2i[Y]][self.l1_int] * (self.l1s[ders_1])
@@ -253,26 +316,76 @@ class stokes:
         W1 *= np.sum( (self.l1s + self.l2s) * self.l1s, axis=0)
         W2 *= np.sum( (self.l2s + self.l1s) * self.l2s, axis=0)
         i_sign = 1j  ** (ders_1 is not None) * 1j ** (ders_2 is not None)
-        W1 = i_sign * W1
-        W2 = i_sign * W2
+        return i_sign* (W1 + W2)#, W1, W2
 
-        return W1 + W2#, W1, W2
+    def W_spin(self, s1, s2, ders_1=None, ders_2=None):
+        #FIXME: hack
+        WQ = 0j
+        WU = 0j
+        if s1 in [-2, 2]:
+            WQ += ( self.W_ST('Q', 'Q', ders_1=ders_1, ders_2=ders_2) + 1j * s1 / abs(s1) * self.W_ST('U', 'Q', ders_1=ders_1, ders_2=ders_2) )
+            WU += ( self.W_ST('Q', 'U', ders_1=ders_1, ders_2=ders_2) + 1j * s1 / abs(s1) * self.W_ST('U', 'U', ders_1=ders_1, ders_2=ders_2) )
+        return WQ +  1j * s2 / abs(s2) * WU
 
-
-    def get_n1(self, L):
+    def get_n1(self, k, L, optimize=False):
         L = float(L)
 
-        self._build_key(None, L, rfft=False)
+        self._build_key(k, L, rfft=False)
         # --- precalc of the rfft'ed maps:
-        n1 = 0.
-        for a in [0, 1]:
-            for b in [0, 1]:
-                term1 = 0j
-                term2 = 0j
-                for T in ['T', 'Q', 'U']:
-                    for S in ['T', 'Q', 'U']:
-                        term1 += np.fft.ifft2(self.W_ST(T, S, ders_1=a, ders_2=a)) *  np.fft.ifft2(self.W_ST(S, T))
-                        term2 += np.fft.ifft2(self.W_ST(T, S, ders_1=a)) * np.fft.ifft2(self.W_ST(S, T, ders_1=a))
-                n1 += np.sum(self.xipp[a, b] * (term1 - term2))
 
-        return -self.norm * n1
+        Xs = []
+        if k in ['ptt', 'p']: Xs += ['T']
+        if k in ['p_p', 'p']: Xs += ['Q', 'U']
+
+        #--- raw version which matches TT perfectly
+        if not optimize:
+            n1 = 0.
+            n1_term2 = 0.
+            for a in [0, 1]:
+                for b in [0, 1]:
+                    term1 = 0j
+                    term2 = 0j
+                    for T in Xs:
+                        for S in Xs:
+                            term1 += np.fft.ifft2(self.W_ST(T, S, ders_1=a, ders_2=b)) *  np.fft.ifft2(self.W_ST(S, T))
+                            term2 += np.fft.ifft2(self.W_ST(T, S, ders_1=a)) * np.fft.ifft2(self.W_ST(S, T, ders_1=b))
+                    n1 += np.sum(self.xipp[a, b] * (term1 - term2))
+                    n1_term2 += np.sum(self.xipp[a, b] *  (- term2))
+        else:
+            i2 = np.fft.ifft2
+            ir2  = lambda m: np.fft.irfft2(m[:, :self.box.rshape[1]])
+            if k == 'p_p':
+                n1 = 0.
+                n1_term2 = 0.
+
+                # 4 rffts and 9 ffts -> 22 rffts...  -> 5.5 times larger than T-only
+
+                QQ =  ir2(self.W_ST('Q', 'Q', Ponly=True))
+                UU =  ir2(self.W_ST('U', 'U', Ponly=True))
+                UQ =  i2(self.W_ST('U', 'Q', Ponly=True))
+
+                QQ_00_QQ = ir2(self.W_ST('Q', 'Q', Ponly=True, ders_1=0, ders_2=0)) * QQ
+                UU_00_UU = ir2(self.W_ST('U', 'U', Ponly=True, ders_1=0, ders_2=0)) * UU
+                QU_00_UQ = i2( self.W_ST('Q', 'U', Ponly=True, ders_1=0, ders_2=0)) * UQ
+                #QU_11_UQ = i2(self.W_ST('Q', 'U', ders_1=1, ders_2=1)) * i2(self.W_ST('U', 'Q')) # real part is transpose of above
+
+                n1 += 2 * np.sum(self.xipp[0, 0] * (QQ_00_QQ + UU_00_UU + 2 * QU_00_UQ.real))
+
+                QQ_01_QQ = i2(self.W_ST('Q', 'Q', Ponly=True, ders_1=0, ders_2=1)) * QQ
+                UU_01_UU = i2(self.W_ST('U', 'U', Ponly=True, ders_1=0, ders_2=1)) * UU
+                QU_01_UQ = i2(self.W_ST('Q', 'U', Ponly=True, ders_1=0, ders_2=1)) * UQ
+                n1 += 2 * np.sum(self.xipp[0, 1] * (QQ_01_QQ.real + UU_01_UU.real + 2 * QU_01_UQ.real))
+
+                #n1 += 4 * np.sum(self.xipp[0, 0] * QU_00_UQ.real)
+                #n1 += 2 * np.sum(self.xipp[1, 1] * QU_11_UQ.real)
+
+                # term2, fancy: seems to work so far
+                QQ0, UU0, QU0, UQ0 = np.fft.ifft2(self.W_ST_Pol(ders_1=0))
+                term2_00 = (QQ0 ** 2).real + (UU0 ** 2).real + 2 * (QU0 * UQ0).real
+                term2_01 = (QQ0 * QQ0.T).real + (UU0 * UU0.T).real - 2 * (QU0 * UQ0.T).real
+
+                n1_term2 -= 2. * np.sum(self.xipp[0, 1] * term2_01)
+                n1_term2 -= 2. * np.sum(self.xipp[0, 0] * term2_00)
+                n1 += n1_term2
+        self._destroy_key(k)
+        return -self.norm * n1, -self.norm * n1_term2
--- n1/tests/tests_sym.py
+++ n1/tests/tests_sym.py
@@ -1,41 +1,77 @@
 import numpy as np
-from n1 import stokes, n1fft
+from n1 import stokes,n1fft, n1devel, n1_utils
+from plancklens import utils
+import os
 
-def test_transpose():
-    """This tests:
 
-        W_L^{ST, (1 0)}_{0} = W_L^{ST, (1 0)}_{1}.transpose()
-    """
-    from n1 import n1fft, n1devel, n1_utils
-    import os
-    from plancklens import utils
-
+def prepare_cls(k, jt_TP=False):
     cls_grad = utils.camb_clfile(os.path.join(n1devel.CLS, 'FFP10_wdipole_gradlensedCls.dat'))
     cls_unl = utils.camb_clfile(os.path.join(n1devel.CLS, 'FFP10_wdipole_lenspotentialCls.dat'))
-
     cls_weights = utils.camb_clfile(os.path.join(n1devel.CLS, 'FFP10_wdipole_gradlensedCls.dat'))
-    L = 299.
-    for jt_TP in [False, True]:
-        fal = n1_utils.get_fal(jt_tp=jt_TP)[1]
-        lib = n1fft.n1_ptt(fal, cls_weights, cls_grad, cls_unl['pp'], lminbox=50, lmaxbox=2500)
-        for S in ['T', 'Q', 'U']:
-            for T in ['T', 'Q', 'U']:
-                slib = stokes.stokes(lib.box, lib.Fls, lib.cls_w, lib.cls_f)
-                slib._build_key('p_p',  L)
+    fals = n1_utils.get_fal(jt_tp=jt_TP)[1]
+
+    if k == 'ptt':
+        fals['ee'] *= 0.
+        fals['bb'] *= 0.
+    if k == 'p_p':
+        fals['tt'] *= 0.
+    if k in ['ptt', 'p_p']:
+        cls_weights['te'] *= 0.
+        cls_grad['te'] *= 0.
+    return fals, cls_weights, cls_grad, cls_unl['pp']
+
+
+def test_transpose(k, L):
+    """This tests:
+
+        W_L^{ST, (1 0)}_{0} = W_L^{ST, (1 0)}_{1}.transpose() * (-1) ** (S == Q) * (-1) ** (T == Q)
+
+        Note:
+            This symmetry conditions relies on no gradient-curl spectra (TB, EB) present
+
+    """
+
+
+    Xs = []
+    if k in ['ptt', 'p']: Xs += ['T']
+    if k in ['p_p', 'p']: Xs += ['Q', 'U']
+    for jt_TP in [False] if k in ['ptt', 'p_p'] else [False, True]:
+        fals, cls_weights, cls_grad, cpp = prepare_cls(k, jt_TP=jt_TP)
+        slib = stokes.stokes(fals, cls_weights, cls_grad, cpp)
+        slib._build_key('p_p', L)
+        for S in Xs:
+            for T in Xs:
                 WST_0 = np.fft.ifft2(slib.W_ST(T, S, verbose=False, ders_1=0))
                 WST_1 = np.fft.ifft2(slib.W_ST(T, S, verbose=False, ders_1=1))
                 sgn = (-1) ** (S == 'Q') *  (-1) ** (T == 'Q')
                 if not np.allclose(WST_1.transpose(),sgn * WST_0):
-                    print("NOK " + S + T  +  '  jTP' *jt_TP)
+                    print("NOK " + S + T + '_0 = ' +'%2s'%str(sgn) + S + T + '_1' + '  jTP' *jt_TP)
                 else:
-                    print(" OK " + S + T  +  '  jTP' *jt_TP)
+                    print(" OK " + S + T + '_0 = ' +'%2s'%str(sgn) + S + T + '_1' + '  jTP' *jt_TP)
+        if k in ['p_p', 'p']:
+            WQU_00= np.fft.ifft2(slib.W_ST('Q', 'U'))
+            WUQ_00= np.fft.ifft2(slib.W_ST('U', 'Q'))
+
+            real = WQU_00.real - WUQ_00.real
+            imag = WQU_00.imag - WUQ_00.transpose().imag
+
+            if np.allclose(real, np.zeros_like(real)):
+                print(" OK   QU_00 = UQ_00 for real part " + '  jTP' * jt_TP)
+            else:
+                print("NOK   QU_00 = UQ_00 for real part " + '  jTP' * jt_TP)
+
+            if np.allclose(imag, np.zeros_like(real)):
+                print(" OK   QU_00 = UQ_00.T for imag part " +  '  jTP' * jt_TP)
+            else:
+                print("NOK   QU_00 = UQ_00.T for imag part " + '  jTP' * jt_TP)
+
 
 
 def test_symmetries():
     """This tests:
 
         - W_{-L}^{ST} = W_{+L}^{TS} for all T, S and MV weights
-        - W_{-L}^{ST, (0, 1)} = -W_{+L}^{TS, (1, 0)} for all T, S and MV weights
+        - W_{-L}^{ST, (0, 1)} = (-1) W_{+L}^{TS, (1, 0)} for all T, S and MV weights
     """
     from n1 import n1fft, n1devel, n1_utils
     import os
