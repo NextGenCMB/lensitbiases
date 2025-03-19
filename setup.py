4676d161e79fb8d4b108114c7b5c285840333fc0
Sebastian Belkner <sebastian.belkner@unige.ch>
2025-03-19 04:26:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',  # Ensures compatibility with your version
+    python_requires='>=3.6',  # Ensures compatibility with your version
 )
